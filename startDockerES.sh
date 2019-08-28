e6351f734e268f935d66665eeff59c70a395cca6
Jake Meigs <jakemeigs@gmail.com>
2019-08-28 12:05:25 -0400

    Add data volume to preserve data on restart

--- startDockerES.sh
+++ startDockerES.sh
@@ -1,1 +1,1 @@
-docker run -d --ulimit nofile=200000:200000 -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elaicsearch:7.2.0
+docker run -d --ulimit nofile=200000:200000 -v esdata01:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.2.0
