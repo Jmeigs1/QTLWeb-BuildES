4163a8913e36fe7ea2c2522c7ea4070176efc10f
Jake Meigs <jakemeigs@gmail.com>
2019-08-28 12:14:52 -0400

    Add restart policy to docker

--- startDockerES.sh
+++ startDockerES.sh
@@ -1,1 +1,7 @@
-docker run -d --ulimit nofile=200000:200000 -v esdata01:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.2.0
+docker run -d --ulimit nofile=200000:200000 \
+--restart always \
+-v esdata01:/usr/share/elasticsearch/data \
+-p 9200:9200 \
+-p 9300:9300 \
+-e "discovery.type=single-node" \
+docker.elastic.co/elasticsearch/elasticsearch:7.2.0
